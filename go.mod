8dd524ae4e4d5d1476bc03a19c8e24ed4e78eb87
shddtr <shddtr@gmail.com>
2025-03-02 16:05:29 +0900

    use my-math/v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module use-my-math
 
 go 1.22.2
 
-require github.com/shddtr/my-math v1.0.0
+require github.com/shddtr/my-math/v2 v2.0.0
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/shddtr/my-math/pkg/double"
-	"github.com/shddtr/my-math/pkg/formatter"
+	"github.com/shddtr/my-math/v2/pkg/doubler"
+	"github.com/shddtr/my-math/v2/pkg/formatter"
 )
 
 func main() {
-	result := double.Double(10)
+	result := doubler.Double(10)
 	fmt.Println(formatter.Format(fmt.Sprintf("%d", result)))
 }
